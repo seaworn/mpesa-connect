b9e8e204365b03fbe345d574206f55ccc503939f
Emz D <seaworndrift@gmail.com>
2025-08-12 00:30:30 +0300

    feat(utils): add utitlity for generating security credential

--- src/mpesa_connect/utils.py
+++ src/mpesa_connect/utils.py
@@ -1,7 +1,11 @@
 import base64
+import logging
+import pathlib
 import re
 from datetime import datetime
 from typing import Union
+
+_logger = logging.getLogger(__name__)
 
 
 def base64encode(data: str) -> str:
@@ -23,3 +27,46 @@ def generate_password(
     timestamp: Union[str, int],
 ) -> str:
     return base64encode(str(business_short_code) + pass_key + str(timestamp))
+
+
+def generate_security_credential(
+    password: str, cer: Union[pathlib.Path, str, bytes]
+) -> str:
+    """
+    Generate security credentials from a PEM certificate
+
+    Args:
+        password (str): The unencrypted initiator password
+        cert (str): X509 certificate - either the path to a file or the PEM string in bytes
+
+    Returns:
+        str: Base64 encoded security credential
+    """
+    try:
+        from cryptography import x509
+        from cryptography.hazmat.primitives.asymmetric import padding
+    except ImportError as e:
+        _logger.error(str(e))
+        raise Exception(
+            "Cryptography library is not installed, please install with `pip install mpesa-connect[cryptography]`"
+        ) from e
+    try:
+        # Read the certificate
+        if isinstance(cer, pathlib.Path):
+            with cer.open("rb") as f:
+                data = f.read()
+        elif isinstance(cer, str):
+            with open(cer, "rb") as f:
+                data = f.read()
+        else:
+            data = cer
+        # Extract public key
+        certificate = x509.load_pem_x509_certificate(data)
+        public_key = certificate.public_key()
+        # Encrypt using RSA with PKCS1v1.5 padding
+        encrypted_bytes = public_key.encrypt(password.encode(), padding.PKCS1v15())
+        # Convert to base64 string
+        return base64.b64encode(encrypted_bytes).decode()
+    except Exception as e:
+        _logger.error(str(e))
+        raise Exception(f"Error generating security credential: {str(e)}") from e
